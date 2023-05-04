3ee354791b073a4429f3184f71ebb49f442fbf21
stayuglyq <Samwise2004@yandex.ru>
2023-05-04 16:57:45 +0300

    second

---
+++ formatter_lib/CMakeFiles/formatterExe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatterExe.dir/formatter.cpp.o"
+  "CMakeFiles/formatterExe.dir/formatter.cpp.o.d"
+  "libformatterExe.a"
+  "libformatterExe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatterExe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello_world_application/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/Users/aynes/projects_git/timp_lab3/lab03/solver_lib/solver.cpp.o"
+  "CMakeFiles/solver.dir/Users/aynes/projects_git/timp_lab3/lab03/solver_lib/solver.cpp.o.d"
+  "libsolver.a"
+  "libsolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
