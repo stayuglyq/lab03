d232c33ed8f9c8d823c6a6b347aeaa432af22e91
stayuglyq <Samwise2004@yandex.ru>
2023-05-04 16:54:36 +0300

    first

---
+++ solver_application/CMakeFiles/formatter_ex_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatter_ex_lib.dir/Users/aynes/projects_git/timp_lab3/lab03/formatter_ex_lib/formatter_ex.cpp.o"
+  "CMakeFiles/formatter_ex_lib.dir/Users/aynes/projects_git/timp_lab3/lab03/formatter_ex_lib/formatter_ex.cpp.o.d"
+  "libformatter_ex_lib.a"
+  "libformatter_ex_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatter_ex_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
